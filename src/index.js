c90f2863c9ef09146859f1957085f897ba46dfbe
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-14 14:34:02 +0300

    Добавляет шрифты

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import 'normalize.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import './fonts/roboto/roboto.css';
+import './fonts/robotoslab/robotoslab.css';
+import './fonts/worksans/worksans.css';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
