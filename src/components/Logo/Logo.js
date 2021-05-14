6620991c9a968f5c17ec4cbac27cf8000ac2539f
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-14 23:25:41 +0300

    Создает лого

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,12 @@
+import logo from './img/logo.svg';
+import './Logo.scss';
+
 const Logo = () => {
-  return <div className="logo"></div>;
+  return (
+    <div className="logo">
+      <img className="logo__img" src={logo} alt="Company logo" />
+    </div>
+  );
 };
 
 export default Logo;
