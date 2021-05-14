6949b159a56b20919583a2aa7c986958e0738bc9
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-14 14:34:49 +0300

    Создает моковые данные

---
+++ src/mock/mock-data.js
@@ -0,0 +1,71 @@
+import bothImg1 from './img/booths/photo1.jpg';
+import bothImg2 from './img/booths/photo2.jpeg';
+import bothImg3 from './img/booths/photo3.jpeg';
+import bothImg4 from './img/booths/photo4.jpeg';
+import bothImg5 from './img/booths/photo5.jpeg';
+
+import newsImg1 from './img/news/news1.jpg';
+import newsImg2 from './img/news/news2.jpeg';
+import newsImg3 from './img/news/news3.jpeg';
+import newsImg4 from './img/news/news4.jpeg';
+import newsImg5 from './img/news/news5.jpg';
+
+const booth = {
+  name: 'Фотобудка с ширмой',
+  photos: [bothImg1, bothImg2, bothImg3, bothImg4, bothImg5],
+  cost: 1700000,
+  size: [2, 1.5, 2],
+  options: null,
+};
+
+const option = {
+  name: 'Разработка макета рамки',
+  photo: bothImg1,
+  cost: 1750000,
+};
+
+const oneNews = {
+  name: 'Фотобудки, GIF стойка',
+  digest: 'У нас есть широчайший ассортимент фотобудок на все случаи жизни',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  category: 'Услуги',
+  date: '20-03-2021',
+  photos: [newsImg1, newsImg2, newsImg3, newsImg4, newsImg5],
+};
+
+const rentalTimes = [
+  { id: 1, name: '1 час', value: 1 },
+  { id: 2, name: '2 часа', value: 2 },
+  { id: 3, name: '3 часа', value: 3 },
+  { id: 4, name: '5 часов', value: 5 },
+  { id: 5, name: 'выставка 2 дня', value: 12 },
+  { id: 6, name: 'выставка 3 дня', value: 13 },
+];
+
+const createMockArray = (mock, number) => {
+  const COST_INCREMENT = 10000;
+  const booths = [];
+
+  for (let i = 0; i < number; i++) {
+    booths.push({
+      ...mock,
+      name: `${mock.name} #${i + 1}`,
+      cost: mock.cost - 10000 + COST_INCREMENT * (i + 1),
+      id: i + 1,
+    });
+  }
+  return booths;
+};
+
+const createMockNews = (mock, number) => {
+  const news = [];
+  for (let i = 0; i < number; i++) {
+    news.push(mock);
+  }
+};
+
+const booths = createMockArray(booth, 20);
+const options = createMockArray(option, 20);
+const news = createMockNews(oneNews, 20);
+
+export { booths, options, rentalTimes, news };
