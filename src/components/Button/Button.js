b7c3bc745ec0834a3b09370a217a9731fc32a2ef
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-18 13:38:13 +0300

    Прячет кнопку, когда нет новостей

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,13 +5,13 @@ export const WIDE_FILL_MOD = 'wide-fill';
 
 const Button = ({ name, onClick, isActive = true, type }) => {
   return (
-    <button
-      className={`button${` button--${type}`}`}
-      disabled={!isActive}
-      onClick={onClick}
-    >
-      {name}
-    </button>
+    <>
+      {isActive && (
+        <button className={`button${` button--${type}`}`} onClick={onClick}>
+          {name}
+        </button>
+      )}
+    </>
   );
 };
 
